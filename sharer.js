1e3021e4978c64094479fe2b7c5b8a4f6f7e7e46
Pierre <pierre@pierre-xps>
2018-02-10 00:48:40 -0500

    pierre sharer first commit

---
+++ sharer.js
@@ -0,0 +1,38 @@
+var scoreBoard = document.getElementById("score");
+
+
+var ctx = canvas.getContext('2d');
+var height = 60;
+var player = {
+	hitbox: new Rectangle(canvas.width/2, canvas.height-height, width, height),
+	color: "green",
+	draw: function() {
+    	this.hitbox.draw(ctx, this.color);
+	}
+	erase: function(){
+		clearRect(ctx, this.hitbox);
+	},
+	moveLeft: function(){
+		this.erase();
+		if (this.hitbox.x <= 10) {
+			this.hitbox.x = 0;
+		} else{
+			this.hitbox.x -= 10;
+		}
+		this.draw();
+	}
+}
+
+document.body.onload = function()
+{
+    player.draw();
+};
+
+
+
+
+
+
+
+
+
